62cade7cff8e2623050cf5268f54bd467f29d4eb
noemis13 <noemischerer13@gmail.com>
2019-04-15 12:16:19 +0100

    basic

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x;
+            //int y;
+            //x = 10;
+            //y = x + 20;
+            //Console.WriteLine("Result: " + y);
+            //Console.ReadLine();
+
+            //String myName;
+            //myName = "Noemi Scherer";
+
+            //var test = " value";
+
+            //Console.WriteLine(myName+ test);
+            //Console.ReadLine();
+
+            Console.WriteLine("Would you preffer1, 2 or 3:");
+            String userValue;
+            userValue = Console.ReadLine();
+
+            String message = "";
+
+            if(userValue == "1")
+            {
+                message = "You choose 1";
+            }else if(userValue == "2")
+            {
+                message = "You choose 2";
+            }else if(userValue == "3")
+            {
+                message = "Yo choose 3";
+            }else
+            {
+                message= "Sorry, wrong number :(";
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
